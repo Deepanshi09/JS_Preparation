da3e97d414353278b9405bae02aed455c72f37e9
Deepanshi Sharma <68199558+Deepanshi09@users.noreply.github.com>
2024-04-07 15:54:02 +0530

    Added JS output questions for Interviews

---
+++ Output_Question_Banks.js.js
@@ -0,0 +1,19 @@
+Q1. console.log(3+3+"7") //67
+
+Q2. console.log("7"+3+3) // 733
+
+Q3. console.log(3 == "3") // true
+
+Q4. console.log(null === undefined) // false 
+
+Q5. console.log(1 + +"1") // 2 bcz +”1” evaluates to 1 as a numerical value.
+
+Q6. console.log(1-"1") // 0 ,bcz -“1” converted as a numeric 1. 
+
+Q7. console.log(1<2<3) // 1<2 = true -> true<3 = true bcz in JS true=1,
+
+Q8. console.log(3>2>1) //   false=0 and Js checks from Left to right. 
+
+Q9. var a1=5;
+    console.log(a1++) // 5, first it will print
+    console.log(a1) //6, after print, increment will happen.
